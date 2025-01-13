3e6205a004b29ac9caccff9398251050420f02db
richhell <richardhellinga@gmail.com>
2025-01-12 20:29:49 -0500

    new approach

--- index.js
+++ index.js
@@ -9,7 +9,61 @@
 
 //Books 
 
+const booksContainer = document.getElementById("books-container")
 
+async function onFetchBooksClick() {
+    const response = await fetch("http://localhost:3000/books")
+    const bookList = await response.json()
+
+    booksContainer.innerHTML = bookList.map(
+        book => `<div class="bg-light rounded mt-5">
+            <h3>${book.title}</h3>
+            <p>${book.genreId}</p>
+        </div>`
+    ).join("")
+}
+
+let lastCreatedItem = null
+
+async function onCreateBookClick() {
+    const testBook = { title: "Test Book", genreId: 2 }
+    const response = await fetch("http://localhost:3000/books", {
+        method: "POST", 
+        headers: { "Content-Type": "application/json" }, 
+        body: JSON.stringify(testBook) 
+    })
+   
+    const newlyCreatedItem = await response.json()
+    lastCreatedItem = newlyCreatedItem
+}
+
+async function onDeleteBookClick() {
+    if(lastCreatedItem === null) {
+        console.log("No item created yet to delete")
+        return
+    }
+    
+    fetch("http://localhost:3000/books/" + lastCreatedItem.id, {
+        method: "DELETE", // delete
+    })
+}
+
+// Genres 
+
+const genresContainer = document.getElementById("genres-container")
+const genreIdTextbox = document.getElementById("genre-id-textbox")
+
+async function onFetchGenresClick() {
+    const response = await fetch("http://localhost:3000/genres")
+    const genreList = await response.json()
+
+    genresContainer.innerHTML = genreList.map(
+        genre => `<div class="bg-light rounded mt-5">
+            <h3>${genre.title}</h3>
+            <p>id: ${genre.id}</p>
+        </div>`
+    ).join("")
+}
 
 
 
